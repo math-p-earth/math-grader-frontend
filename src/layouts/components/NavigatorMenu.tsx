1a15225827f203b75fc21c54a999def077a8989c
Parakorn Leknamongkol <parakorn.lk@gmail.com>
2022-10-15 14:58:20 +0700

    [feat](nav): add menulist

--- src/layouts/components/NavigatorMenu.tsx
+++ src/layouts/components/NavigatorMenu.tsx
@@ -1,4 +1,84 @@
-import { Box, Drawer } from "@mui/material";
+import {
+    Box,
+    Divider,
+    Drawer,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Paper,
+} from "@mui/material";
+import { theme } from "../../assets/styles/theme";
+import HomeIcon from "@mui/icons-material/Home";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import DescriptionIcon from "@mui/icons-material/Description";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+
+interface MenuListInterface {
+    icon: JSX.Element;
+    label: string;
+    link: string;
+}
+
+const menuListArr = [
+    [
+        {
+            icon: <HomeIcon />,
+            label: "Home",
+        },
+        {
+            icon: <MenuBookIcon />,
+            label: "Lecture Problem",
+        },
+        {
+            icon: <ListAltIcon />,
+            label: "Problem Set",
+        },
+        {
+            icon: <DescriptionIcon />,
+            label: "Past Paper",
+        },
+    ],
+    [
+        {
+            icon: <div />,
+            label: "Training System",
+        },
+    ],
+] as MenuListInterface[][];
+
+function MenuList() {
+    const MenuMap = (arr: MenuListInterface[]) => {
+        return arr.map((item) => {
+            return (
+                <ListItem>
+                    <ListItemButton
+                        component={Paper}
+                        style={{
+                            borderRadius: "20px",
+                        }}
+                    >
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText>{item.label}</ListItemText>
+                    </ListItemButton>
+                </ListItem>
+            );
+        });
+    };
+
+    return (
+        <List dense>
+            {MenuMap(menuListArr[0])}
+            <Divider variant="middle" sx={{ pt: "6px", pb: "6px" }} />
+            {MenuMap(menuListArr[1])}
+        </List>
+    );
+}
+
+// Home
+// Drill
+// Past Paper Collection
 
 export function NavigatorMenu({
     menuWidth,
@@ -21,7 +101,19 @@ export function NavigatorMenu({
             anchor="left"
             open={open}
         >
-            <Box></Box>
+            <Paper elevation={4}>
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="flex-end"
+                    padding={theme.spacing(0, 1)}
+                    sx={{
+                        ...theme.mixins.toolbar,
+                        backgroundColor: theme.palette.primary.main,
+                    }}
+                />
+            </Paper>
+            <MenuList />
         </Drawer>
     );
 }
