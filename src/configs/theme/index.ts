ec9c8b4b4ccbc041c400ba1bd7bc006053a9d6aa
Parakorn Leknamongkol <parakorn.lk@gmail.com>
2023-01-13 00:08:23 +0700

    [fix]: fix the background color

--- src/configs/theme/index.ts
+++ src/configs/theme/index.ts
@@ -1,3 +1,17 @@
+declare module '@mui/material/styles' {
+  interface Theme {
+    colorcode: {
+      red: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    colorcode?: {
+      red?: string;
+    };
+  }
+}
+
 import { PaletteOptions, colors, createTheme } from '@mui/material'
 
 export const palette: PaletteOptions = {
@@ -7,8 +21,21 @@ export const palette: PaletteOptions = {
   secondary: {
     main: colors.orange[500],
   },
+  background: {
+    default: colors.grey[400]
+  },
+  mode: 'dark',
 }
 
 export const theme = createTheme({
   palette: palette,
+  colorcode: {
+    red: '#FF6B6B'
+  }
 })
+
+
+// 
+// #FFD93D
+// #6BCB77
+// #4D96FF
