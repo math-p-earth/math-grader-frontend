6b141202d38b212cf82a36f47d0bc9ed57881f39
Parakorn Leknamongkol <parakorn.lk@gmail.com>
2023-01-28 21:39:58 +0700

    [clean] remove dto

--- src/types/dto.tsx
+++ src/types/dto.tsx
@@ -74,41 +74,6 @@ export interface Source {
   updatedAt: string
 }
 
-export interface ProblemDto {
-  id: string
-  type: ProblemType
-  content: string
-  answer: string
-  choices: string[]
-  order?: number
-  score?: number
-  source?: string
-}
-
-export interface SourceDto {
-  id: string
-  name: string
-  description: string
-  type: SourceType
-  book: {
-    author: string
-    isbn: string
-  }
-  paper: {
-    timeLimit: number
-    datePublished: Date
-  }
-  length: number
-}
-
-export interface ProblemListDto {
-  id: string
-  name: string
-  description: string
-  type: ProblemListType
-  length: number
-}
-
 export type ProblemType = 'MCQ' | 'SHORT' | 'TF' | 'PROOF'
 export type SourceType = 'BOOK' | 'PAPER'
 export type ProblemListType = 'DRILL' | 'LECTURE_PROBLEM' | 'COLLECTION' | 'CHALLENGE'
