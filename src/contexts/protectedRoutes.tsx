01bfcb3f0c05bffbb74f3270a02ce133d53ae451
Nut Pinyo <bomb.np@gmail.com>
2023-04-08 00:17:24 +0700

    wrap protected routes in useEffect

--- src/contexts/protectedRoutes.tsx
+++ src/contexts/protectedRoutes.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import { useRouter } from 'next/router'
 
 import { useUser } from '../hooks/useUser'
@@ -11,19 +13,22 @@ const unprotectedRoutes = ['/login', '/user/pending-approval', '/user/register-s
 export const ProtectedRoutes: React.FC<ProtectedRoutesProps> = ({ children }) => {
   const { user, isLoading } = useUser()
   const router = useRouter()
-  if (isLoading) {
-    return null
-  }
 
-  if (!unprotectedRoutes.includes(router.pathname)) {
-    if (!user) {
-      router.push('/login')
-      return null
+  useEffect(() => {
+    if (isLoading) {
+      return
     }
-    if (user.status !== 'APPROVED') {
-      router.push('/user/pending-approval')
-      return null
+    if (!unprotectedRoutes.includes(router.pathname)) {
+      if (!user) {
+        router.push('/login')
+        return
+      }
+      if (user.status !== 'APPROVED') {
+        router.push('/user/pending-approval')
+        return
+      }
     }
-  }
+  }, [isLoading, router, user])
+
   return children
 }
