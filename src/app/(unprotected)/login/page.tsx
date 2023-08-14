02b8440a95766c223343e7c4dde74a4dfbd52f03
Nut Pinyo <bomb.np@gmail.com>
2023-08-14 15:22:40 +0700

    feat: move google login

--- src/app/(unprotected)/login/page.tsx
+++ src/app/(unprotected)/login/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import { GoogleLoginButton } from './components/GoogleLogin'
+import { GoogleLoginButton } from './GoogleLogin'
 
 export default function LoginPage() {
   return (
