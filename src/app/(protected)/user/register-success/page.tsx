7e8c4c604857b839c06f4f5d2be2e3a4565f26eb
Nut Pinyo <bomb.np@gmail.com>
2023-08-14 16:48:03 +0700

    fix: register success font

--- src/app/(protected)/user/register-success/page.tsx
+++ src/app/(protected)/user/register-success/page.tsx
@@ -9,12 +9,12 @@ export default async function RegisterSuccessPage() {
     redirect('/login')
   }
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4">
-      <div className="flex items-center gap-4">
+    <div className="flex h-screen flex-col items-center gap-4">
+      <div className="mb-12 mt-64 flex items-center gap-4">
         <Image src="/images/logos/app.png" alt="App logo" width={100} height={100} />
         <span className="text-3xl font-semibold text-zinc-900">Math P' Earth</span>
       </div>
-      <h2>✅ Success!</h2>
+      <p className="text-2xl font-bold">✅ Success!</p>
       <p>
         Your account,
         <span className="font-semibold">{` ${user.firstName} ${user.lastName}`}</span>, has been
