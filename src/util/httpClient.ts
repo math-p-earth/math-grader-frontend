da6a556b60ad022b7635cceae813719edbf54f80
Nut Pinyo <bomb.np@gmail.com>
2023-09-11 00:34:56 +0700

    refactor: remove old code and dependencies

--- src/util/httpClient.ts
+++ src/util/httpClient.ts
@@ -4,8 +4,3 @@ export const httpClient = axios.create({
   withCredentials: true,
   baseURL: `/api`, // send requests to api proxy
 })
-
-export async function swrFetcher(path: string, params?: unknown) {
-  const response = await httpClient.get(path, { params })
-  return response.data
-}
