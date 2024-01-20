faff1f47b2eac5deec62b37f9d449e50cb0ac3e8
Nut Pinyo <bomb.np@gmail.com>
2024-01-20 20:09:09 +0700

    feat: move web's latex markdown to core

--- packages/core/components/LatexMarkdown/index.tsx
+++ packages/core/components/LatexMarkdown/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { PluggableList } from 'react-markdown/lib/react-markdown'
 
 import { cn } from 'ui/lib/utils'
 
@@ -6,18 +9,16 @@ import { cn } from 'ui/lib/utils'
 // TODO: do normal imports when we switch to Vite with ESNext target
 const ReactMarkdown = React.lazy(() => import('react-markdown'))
 
-let rehypeKatex: (typeof import('rehype-katex'))['default']
-let remarkFrontmatter: (typeof import('remark-frontmatter'))['default']
-let remarkGfm: (typeof import('remark-gfm'))['default']
-let remarkMath: (typeof import('remark-math'))['default']
-;(async () => {
-	if (typeof window !== 'undefined') {
-		rehypeKatex = (await import('rehype-katex')).default
-		remarkFrontmatter = (await import('remark-frontmatter')).default
-		remarkGfm = (await import('remark-gfm')).default
-		remarkMath = (await import('remark-math')).default
-	}
-})()
+type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[]
+	? ElementType
+	: never
+
+export interface ReactMarkdownPlugins {
+	rehypeKatex: ArrayElement<PluggableList>
+	remarkFrontmatter: ArrayElement<PluggableList>
+	remarkGfm: ArrayElement<PluggableList>
+	remarkMath: ArrayElement<PluggableList>
+}
 
 interface LatexMarkdownProps {
 	children: string
@@ -27,13 +28,43 @@ interface LatexMarkdownProps {
 	}
 }
 
+// using dynamic import because these modules are ESM-only
+// TODO: do normal imports when we switch to Vite with ESNext target
+async function loadPlugins(): Promise<ReactMarkdownPlugins> {
+	const [rehypeKatexModule, remarkFrontmatterModule, remarkGfmModule, remarkMathModule] = await Promise.all([
+		import('rehype-katex'),
+		import('remark-frontmatter'),
+		import('remark-gfm'),
+		import('remark-math'),
+	])
+	return {
+		rehypeKatex: rehypeKatexModule.default,
+		remarkFrontmatter: remarkFrontmatterModule.default,
+		remarkGfm: remarkGfmModule.default,
+		remarkMath: remarkMathModule.default,
+	}
+}
+
 export function LatexMarkdown({ children, className, options }: LatexMarkdownProps) {
+	const [plugins, setPlugins] = useState<ReactMarkdownPlugins | null>(null)
+
+	useEffect(() => {
+		loadPlugins().then((result) => {
+			setPlugins(result)
+		})
+	}, [])
+
+	if (!plugins) {
+		// TODO: add loading state?
+		return null
+	}
+
 	const featureClasses = [...(options?.disablePMarginBottom ? ['[&_p]:pb-0'] : ['[&_p]:pb-4'])]
 	return (
 		<ReactMarkdown
 			className={cn('prose dark:prose-invert dark:text-foreground max-w-none', ...featureClasses, className)}
-			rehypePlugins={[rehypeKatex]}
-			remarkPlugins={[remarkMath, remarkGfm, remarkFrontmatter]}
+			rehypePlugins={[plugins.rehypeKatex]}
+			remarkPlugins={[plugins.remarkMath, plugins.remarkGfm, plugins.remarkFrontmatter]}
 		>
 			{children}
 		</ReactMarkdown>
