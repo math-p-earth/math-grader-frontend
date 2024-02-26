59e52062dbbd224805a1718e13871dc4a0d3f2ac
Nut Pinyo <bomb.np@gmail.com>
2024-02-26 18:20:27 +0700

    fix: better tooltip content

--- apps/web/src/components/ProblemCard/SubmissionStatus.tsx
+++ apps/web/src/components/ProblemCard/SubmissionStatus.tsx
@@ -14,40 +14,30 @@ export function SubmissionStatus({ status, size = 16 }: SubmissionStatusProps) {
 	}
 	switch (status) {
 		case 'CORRECT_APPROVED':
-			return (
-				<>
-					<CheckCircle size={size} className="text-green-700" />
-				</>
-			)
+			return <CheckCircle size={size} className="text-green-700" />
 		case 'CORRECT':
 			return (
-				<>
+				<Tooltip
+					content="The submission's answer is correct, but has not yet been manually approved by instructor."
+					className="flex cursor-pointer items-center gap-2"
+				>
 					<Check size={size} className="text-green-700" />
-					<Tooltip content="Pending final check." className="cursor-pointer">
-						<CircleDashed size={size} className="text-orange-400" />
-					</Tooltip>
-				</>
+					<CircleDashed size={size} className="text-orange-400" />
+				</Tooltip>
 			)
 		case 'INCORRECT_APPROVED':
-			return (
-				<>
-					<XCircle size={size} className="text-red-500" />
-				</>
-			)
+			return <XCircle size={size} className="text-red-500" />
 		case 'INCORRECT':
 			return (
-				<>
+				<Tooltip
+					content="The submission's answer is incorrect, but has not yet been manually approved by instructor."
+					className="flex cursor-pointer items-center gap-2"
+				>
 					<X size={size} className="text-red-500" />
-					<Tooltip content="Pending final check." className="cursor-pointer">
-						<CircleDashed size={size} className="text-orange-400" />
-					</Tooltip>
-				</>
+					<CircleDashed size={size} className="text-orange-400" />
+				</Tooltip>
 			)
 		case 'PENDING':
-			return (
-				<>
-					<CircleDashed size={size} className="text-orange-400" />
-				</>
-			)
+			return <CircleDashed size={size} className="text-orange-400" />
 	}
 }
